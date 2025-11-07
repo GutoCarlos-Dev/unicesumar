7140de5ba80eb712c8ae9f4bb3b453a5260b98f7
GutoCarlos-Dev <gutoapollo@hotmail.com>
2025-11-06 22:29:04 -0300

    1

---
+++ semana 4/exerc4.c
@@ -0,0 +1,45 @@
+/*
+    DESAFIO!!!
+
+    Escreva um programa em C que receba 5 números inteiros em um vetor.
+    O programa deverá exibir na tela os 5 números em ordem crescente.
+
+    Exemplo:
+    Recebe: 10 6 7 2 8
+    Escreve: 2 6 7 8 10
+
+    DICA: Pesquisem sobre Bubble Sort.
+*/
+
+#include <stdio.h>
+
+int main() {
+    int numeros[5];
+    int i, j, aux;
+
+    // Recebe os 5 números
+    printf("Digite 5 numeros inteiros:\n");
+    for (i = 0; i < 5; i++) {
+        scanf("%d", &numeros[i]);
+    }
+
+    // Ordena os números usando o método da bolha (bubble sort)
+    for (i = 0; i < 4; i++) {
+        for (j = i + 1; j < 5; j++) {
+            if (numeros[i] > numeros[j]) {
+                aux = numeros[i];
+                numeros[i] = numeros[j];
+                numeros[j] = aux;
+            }
+        }
+    }
+
+    // Exibe os números em ordem crescente
+    printf("Números em ordem crescente:\n");
+    for (i = 0; i < 5; i++) {
+        printf("%d ", numeros[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
